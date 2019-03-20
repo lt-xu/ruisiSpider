75b8ad09afe8e60693c1816f2f381ae47cfc916c
SparkOfLife <ltxu1@outlook.com>
2019-03-20 18:45:28 +0800

    add HtmlDownloader

---
+++ HtmlDownloader.py
@@ -0,0 +1,27 @@
+# encoding: utf-8
+'''
+@author: ltxu
+@file: HtmlDownloader.py
+@time: 19-3-20 下午6:37
+@desc:
+'''
+
+import requests
+
+class HtmlDownloader:
+    def download(self,url):
+        if url is None:
+            return None
+        user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
+        headers={'User-Agent':user_agent}
+        r = requests.get(url,headers=headers)
+        if r.status_code==200:
+            r.encoding='utf-8'
+            return r.text
+        return None
+
+
+if __name__ == '__main__':
+    test = HtmlDownloader()
+    result = test.download('http://rs.xidian.edu.cn/forum.php?mod=forumdisplay&fid=560&filter=typeid&typeid=99')
+    print(result)
